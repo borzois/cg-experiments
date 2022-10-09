a99ad0d34eabc9706a8eb3f72b988e45bccf86f7
borzois <36570861+borzois@users.noreply.github.com>
2022-10-09 12:16:33 +0300

    refactored

--- minimal-ppm.cpp
+++ minimal-ppm.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <vector>
 
 /* Minimal ppm renderer
- * Draws an ugly ass gradient bg
- * Usage: ./minimal-img > out.ppm
+ * Draws an ugly ass gradient background
+ * Usage: ./minimal-img [w] [h] > out.ppm
  */
 
 struct vec3 {
@@ -11,47 +12,75 @@ struct vec3 {
     int b;
 };
 
-void draw_bg(vec3 canvas[256][256], int image_height, int image_width) {
-    for (int j = image_height-1; j >= 0; j--) {
-        for (int i = 0; i < image_width; i++) {
-            auto r = 1 - double(j) / (image_height-1);
-            auto g = 1 - double(j) / (image_height-1);
-            auto b = 1;
+struct canvas_size {
+    int w;
+    int h;
+};
 
-            // converts the color values from 0.0-1.0 floats to 0-255 ints
-            int ir = static_cast<int>(r * 255.999);
-            int ig = static_cast<int>(g * 255.999);
-            int ib = static_cast<int>(b * 255.999);
+void draw_bg(std::vector<std::vector<vec3>> &canvas, const canvas_size csize) {
+    int image_width = csize.w; int image_height = csize.h;
 
+    for (int i = image_height - 1; i >= 0; i--) {
+        // generates the gradient (pretty sure i stole this part from like raytracing in one weekend)
+        auto r = 1 - double(i) / (image_height - 1);
+        auto g = 1 - double(i) / (image_height - 1);
+        auto b = 1;
+
+        // converts the color values from 0.0-1.0 floats to 0-255 ints
+        int ir = static_cast<int>(r * 255.999);
+        int ig = static_cast<int>(g * 255.999);
+        int ib = static_cast<int>(b * 255.999);
+
+        for (int j = 0; j < image_width; j++) {
             canvas[i][j].r = ir;
             canvas[i][j].g = ig;
             canvas[i][j].b = ib;
         }
     }
+    std::cerr << "BG generated\n";
 }
 
-void render(vec3 canvas[256][256], int image_height, int image_width) {
+void render(const std::vector<std::vector<vec3>> &canvas, const canvas_size csize) {
+    int image_width = csize.w; int image_height = csize.h;
+
+    // writes the header
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-    for (int j = image_height-1; j >= 0; j--) {
-        for (int i = 0; i < image_width; i++) {
+
+    // writes the pixel data
+    for (int i = image_height - 1; i >= 0; i--) {
+        for (int j = 0; j < image_width; j++) {
             std::cout << canvas[i][j].r << ' ' << canvas[i][j].g << ' ' << canvas[i][j].b << '\n';
         }
     }
+    std::cerr << "Render finished\n";
+}
+
+canvas_size parse_size(int argc, char** argv) {
+    if (argc >= 3) {
+        return canvas_size {
+            static_cast<int>(strtol(argv[1], nullptr, 10)),
+            static_cast<int>(strtol(argv[2], nullptr, 10))
+        };
+    }
+    return canvas_size { 256, 256 }; // default
 }
 
 int main(int argc, char** argv)
 {
-    const int image_width = 256;
-    const int image_height = 256;
-    vec3 canvas[image_width][image_height];
+    // gets the size of the image
+    canvas_size csize = parse_size(argc, argv);
+    std::cerr << "W: " << csize.w << " H: " << csize.h << '\n';
 
-    // prepare canvas
-    draw_bg(canvas, image_height, image_width);
+    // initializes a 2d vector made of rgb values
+    std::vector<std::vector<vec3>> canvas(csize.h, std::vector<vec3>(csize.w));
 
-    /* stuff goes here */
+    // prepares canvas
+    draw_bg(canvas, csize);
 
-    // write to file
-    render(canvas, image_height, image_width);
+    /* --- stuff goes here --- */
+
+    // writes to file
+    render(canvas, csize);
 
     return 0;
 }
